bae0f35bfa8122a88a08647ecda4db30e3f8a19d
Luiz-Zanoni <z.luiz@escola.pr.gov.br>
2025-08-07 12:16:44 -0300

    Add files via upload

---
+++ terminado/card/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Pokemon',
+    'Como evoluir um Haunter para Gengar?',
+    'O Haunter só evoluir para Gengar por meio de troca de Pokemon'
+)
+
+criaCartao(
+    'Pokemon',
+    'Como evoluir um Riolu?',
+    'Você precisa aumentar o nivel de amizade dele ao máximo e evoluilo durante o dia'
+)
+
+criaCartao(
+    'Pokemon',
+    'Quem é o Deus dos Pokemon?',
+    'O Deus dos Pokemon é o Arceus'
+)
+
+criaCartao(
+    'Pokemon',
+    'Qual o nome dos Pokemon que fazem parte do trio da criação?',
+    'O nome dos Pokemon que fazem parte do trio da criação são: Giratina, Palkia e Dialga'
+)
+
+criaCartao(
+    'Pokemon',
+    'Qual a Pokebola mais roubada do Pokemon',
+    'A Pokebola mais roubada do Pokemon é a Master Ball, com taca de captura de 100%'
+)
